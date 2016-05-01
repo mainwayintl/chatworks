53d891955a8d19e36571b7e1e7ee42a894877599
amos <amos@acloud.co.nz>
2016-05-01 18:46:50 -0400

    Add body parser to the express server so it can parse the request body which is usually in JSON format.

--- api/app.js
+++ api/app.js
@@ -1,8 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+var messages = [
+  {
+    sentDateTime: 'Beginning of time',
+    userMessage: 'Welcome.'
+  }
+];
+
+app.get('/message', function (req, res) {
+  res.send(messages);
+  console.info('get request');
+});
+
+app.post('/message', function (req, res) {
+  console.info('req.body:' + req.body);
+  var newMessage = req.body;
+  messages.push(newMessage);
+  res.send({index: messages.length - 1});
+  console.info('post request');
+});
+
+app.delete('/message', function (req, res) {
+  res.send('removing ...');
+  console.info('remove request');
+});
+
+app.put('/message', function (req, res) {
+  res.send('updating ...');
+  console.info('update request');
 });
 
 app.listen(8080, function () {
