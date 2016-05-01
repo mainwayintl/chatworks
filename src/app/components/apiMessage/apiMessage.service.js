37c68f47418bee9c3392b9501252d74ca0019291
amos <amos@acloud.co.nz>
2016-05-01 18:47:41 -0400

    Create an api message service so our controller can send request for getting all messages from express server as well as add new message to it.

---
+++ src/app/components/apiMessage/apiMessage.service.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ngExercise')
+    .factory('apiMessage', githubContributor);
+
+  /** @ngInject */
+  function githubContributor($log, $http) {
+    var apiHost = 'http://localhost:3000/api';
+
+    var service = {
+      apiHost: apiHost,
+      getAll: getAll,
+      add: add
+    };
+
+    return service;
+
+    function getAll(limit) {
+      if (!limit) {
+        limit = 30;
+      }
+
+      return $http.get(apiHost + '/message?per_page=' + limit)
+        .then(getMessagesComplete)
+        .catch(getMessagesFailed);
+
+      function getMessagesComplete(response) {
+        return response.data;
+      }
+
+      function getMessagesFailed(error) {
+        $log.error('XHR Failed for getAll.\n' + angular.toJson(error.data, true));
+      }
+    }
+
+    function add(newMessage){
+      var data = JSON.stringify(newMessage);
+      $http.post(apiHost + '/message', data);
+    }
+  }
+})();
---
+++ src/app/components/apiMessage/apiMessage.service.spec.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  describe('service apiMessage', function() {
+    var apiMessage;
+    var $httpBackend;
+    var $log;
+
+    beforeEach(module('ngExercise'));
+    beforeEach(inject(function(_apiMessage_, _$httpBackend_, _$log_) {
+      apiMessage = _apiMessage_;
+      $httpBackend = _$httpBackend_;
+      $log = _$log_;
+    }));
+
+    it('should be registered', function() {
+      expect(apiMessage).not.toEqual(null);
+    });
+
+    describe('apiHost variable', function() {
+      it('should exist', function() {
+        expect(apiMessage.apiHost).not.toEqual(null);
+      });
+    });
+
+    describe('getAll function', function() {
+      it('should exist', function() {
+        expect(apiMessage.getAll).not.toEqual(null);
+      });
+
+      it('should return data', function() {
+        $httpBackend.when('GET',  apiMessage.apiHost + '/message?per_page=1').respond(200, [{userMessage: 'Hello.'}]);
+        var data;
+        apiMessage.getAll(1).then(function(fetchedData) {
+          data = fetchedData;
+        });
+        $httpBackend.flush();
+        expect(data).toEqual(jasmine.any(Array));
+        expect(data.length === 1).toBeTruthy();
+        expect(data[0]).toEqual(jasmine.any(Object));
+      });
+
+    });
+  });
+})();
